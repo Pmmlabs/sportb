148eefc47c6f99575e8478ae2237d3424d25b38e
pmmlabs <kilowatt36@mail.ru>
2016-11-13 14:12:30 +0300

    links update

--- sb.php
+++ sb.php
@@ -9,7 +9,7 @@ if (!isset($_GET['m3u'])){
     <title>Sportb</title>
 </head>
 <body>
-<?
+<?php
 }
 
 function curl($url)
@@ -25,34 +25,36 @@ if (isset($_GET['id'])) {
     $json = json_decode(curl("http://news.sportbox.ru/api2/videostream?id=" . $_GET['id'] . "&app_id=android%2F3"), true);
     $url = $json['streams']['url'];
     if (isset($_GET['video'])) { ?>
-        <h1><?=$json['streams']['id'] ?>:<?=$json['streams']['name'] ?></h1>
+        <h1><?=$json['streams']['id'] ?>:HD</h1>
+        <video controls="true" src="<?= str_replace('_1','_3',$url) ?>"></video> <br>
+        <h1><?=$json['streams']['id'] ?>:SD</h1>
         <video controls="true" src="<?= $url ?>"></video> <br>
-    <? } else if (isset($_GET['m3u'])) {
+    <?php } else if (isset($_GET['m3u'])) {
         header('Content-Type: application/vnd.apple.mpegurl');
         ?>
 #EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-STREAM-INF:BANDWIDTH=1,CODECS="avc1.66.31,mp4a.40.2",RESOLUTION=1280x720
-<? echo str_replace('playlist.m3u8', 'chunklist.m3u8', $url);
+<?php echo str_replace('playlist.m3u8', 'chunklist.m3u8', $url);
     }
 } else {
     ?>
     <ol type="1">
-        <?
+        <?php
         $videos = json_decode(curl("http://news.sportbox.ru/api2/rubricvideo?term_id=7212&app_id=android%2F3&page_size=100"), true);
         foreach ($videos['nodes'] as $video) {
-            $id = $video['video']['streams'][2]['id'];
-            $id_sd = $video['video']['streams'][1]['id'];
+            $id = $video['video']['streams'][1]['id'];
+            //$id_auto = $video['video']['streams'][2]['id'];
             ?>
             <li>
                 <?=($video['media_state'] == 'live' ? '[LIVE] ' : '') ?><?= $video['title'] ?>
-                <a href="?id=<?= $id ?>&video">HD Video</a> | <a href="?id=<?= $id ?>&m3u">HD M3U</a> | <a href="?id=<?= $id_sd ?>&video">SD Video</a> | <a href="?id=<?= $id_sd ?>&m3u">SD M3U</a> </li>
-        <? } ?>
+                <a href="?id=<?= $id ?>&video">Video</a></li>
+        <?php } ?>
     </ol>
-    <?
+    <?php
 }
 if (!isset($_GET['m3u'])){ ?>
 </body>
-<?
+<?php
 }
 ?>
