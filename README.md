e7907d8f46ce781ddcb469b3f26402f615c724f5
pmmlabs <kilowatt36@mail.ru>
2016-11-14 23:59:50 +0300

    M3U patching

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 # Описание
 На главной странице выводится список из 100 актуальных видео, и у каждого из них две ссылки:
 
-1. ***Video***: генерируется страница, содержащая ссылку на HD-трансляцию внутри тега `<video/>`, которую можно отправить на Chromecast через приложение [Web Video Cast](https://play.google.com/store/apps/details?id=com.instantbits.cast.webvideo). Это рекомендованный вариант.
-1. ***M3U***: генерируется плейлист формата M3U, эмулирующий playlist.m3u8 (для обхода оригинального playlist.m3u8)
+1. ***Video***: генерируется страница, содержащая ссылку на HD-трансляцию внутри тега `<video/>`, которую можно отправить на Chromecast через приложение [Web Video Cast](https://play.google.com/store/apps/details?id=com.instantbits.cast.webvideo).
+1. ***M3U***: генерируется плейлист формата M3U, эмулирующий playlist.m3u8 (для обхода оригинального playlist.m3u8). Это рекомендованный вариант для Chromecast, т.к. в оригинальном потоке запрещено кеширование.
 
 # Скриншот
 ![screenshot](https://cloud.githubusercontent.com/assets/2682026/16022716/51dee470-31ca-11e6-8a14-0363d37e28be.png)
--- sb.php
+++ sb.php
@@ -1,5 +1,8 @@
 <?php
-if (!isset($_GET['m3u'])){
+$M3U_MODE = isset($_GET['m3u']) || isset($_GET['chunklist']);
+if ($M3U_MODE) {
+    header('Content-Type: application/vnd.apple.mpegurl');
+} else {
 ?>
 <!DOCTYPE html>
 <html lang="ru" dir="ltr" class="client-nojs">
@@ -30,13 +33,29 @@ if (isset($_GET['id'])) {
         <h1><?=$json['streams']['id'] ?>:SD</h1>
         <video controls="true" src="<?= $url ?>"></video> <br>
     <?php } else if (isset($_GET['m3u'])) {
-        header('Content-Type: application/vnd.apple.mpegurl');
-        ?>
-#EXTM3U
-#EXT-X-VERSION:3
-#EXT-X-STREAM-INF:BANDWIDTH=1,CODECS="avc1.66.31,mp4a.40.2",RESOLUTION=1280x720
-<?php echo str_replace('playlist.m3u8', 'chunklist.m3u8', $url);
+        $playlist = curl($url);
+        $streams = array();
+        foreach (preg_split('/#EXT-X-STREAM-INF/', $playlist) as $stream) {
+            if (preg_match('/BANDWIDTH=(\d+)/', $stream, $matches)) {
+                $bitrate = $matches[1];
+                if (preg_match('/\S+chunklist\.m3u8.*/', $stream, $matches)) {
+                    $streams[$bitrate] = $matches[0];
+                }
+            }
+        }
+        krsort($streams);
+        $best_chunklist = array_shift($streams);
+        print "#EXTM3U\n"
+            . "#EXT-X-VERSION:3\n"
+            . "#EXT-X-STREAM-INF:PROGRAM-ID=0,BANDWIDTH=1\n"
+            . basename($_SERVER["SCRIPT_NAME"]) . '?chunklist=' . urlencode($best_chunklist);
     }
+} else if (isset($_GET['chunklist'])) {
+    $chunklist_url = urldecode($_GET['chunklist']);
+    $chunklist = curl($chunklist_url);
+    $chunklist = str_replace('#EXT-X-ALLOW-CACHE:NO', '#EXT-X-ALLOW-CACHE:YES', $chunklist);
+    $chunklist = str_replace('media', dirname($chunklist_url).'/media', $chunklist);
+    print $chunklist;
 } else {
     ?>
     <ol type="1">
@@ -44,16 +63,16 @@ if (isset($_GET['id'])) {
         $videos = json_decode(curl("http://news.sportbox.ru/api2/rubricvideo?term_id=7212&app_id=android%2F3&page_size=100"), true);
         foreach ($videos['nodes'] as $video) {
             $id = $video['video']['streams'][1]['id'];
-            //$id_auto = $video['video']['streams'][2]['id'];
+            $id_auto = $video['video']['streams'][2]['id'];
             ?>
             <li>
                 <?=($video['media_state'] == 'live' ? '[LIVE] ' : '') ?><?= $video['title'] ?>
-                <a href="?id=<?= $id ?>&video">Video</a></li>
+                <a href="?id=<?= $id ?>&video">Video</a> | <a href="?id=<?= $id_auto ?>&m3u">M3U</a></li>
         <?php } ?>
     </ol>
     <?php
 }
-if (!isset($_GET['m3u'])){ ?>
+if (!$M3U_MODE){ ?>
 </body>
 <?php
 }
